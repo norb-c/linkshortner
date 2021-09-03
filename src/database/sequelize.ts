ec7439b050bc19783ddb64bd6a507f1f495b1f10
Chigozie Madubuko <apple3@Chigozies-MacBook-Pro.local>
2021-09-03 22:46:05 +0100

    clean ups

--- src/database/sequelize.ts
+++ src/database/sequelize.ts
@@ -5,8 +5,12 @@ const databaseConfig: Options = require('../config/mysql')[process.env.NODE_ENV 
 const { database, username, password, ...sequelizeDatabaseConfig } = databaseConfig;
 let sequelize: Sequelize;
 if (process.env.NODE_ENV === 'production') {
+  console.log('In production mode ....', process.env.DATABASE_URL);
+
   sequelize = new Sequelize(process.env.DATABASE_URL, sequelizeDatabaseConfig);
 } else {
+  console.log('In development mode ....');
+
   sequelize = new Sequelize(database, username, password, sequelizeDatabaseConfig);
 }
 export { sequelize };
